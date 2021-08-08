9ebfe740a00e21ae8c13b785dada2b31dbc50064
sibe57 <yqtwre1234@yandex.ru>
2021-08-08 23:50:21 +0300

    add 67 and 18

--- problem67.py
+++ problem67.py
@@ -1,22 +1,20 @@
-t=open('/home/sibe57/PycharmProjects/Euler-Problems2/triangle.txt')
+t=open('triangle.txt')
 text=[]
 for i in t:
     i=i[:-1]
-    liner=i.replace(' ',',')
-    liner=liner.split(',')
+    liner=i.split(' ')
     for g in range(len(liner)):
         liner[g]=int(liner[g])
-
     text.append(liner)
 print(text)
 text.reverse()
-perfect_triangle=text
-for i in range(1,len(perfect_triangle)):
-    for g in range(0,len(perfect_triangle[i])):
-        left = perfect_triangle[i][g]+perfect_triangle[i-1][g]
-        right = perfect_triangle[i][g] + perfect_triangle[i-1][g+1]
+triangle=text
+for i in range(1, len(triangle)):
+    for g in range(0, len(triangle[i])):
+        left = triangle[i][g] + triangle[i - 1][g]
+        right = triangle[i][g] + triangle[i - 1][g + 1]
         if left>right:
-            perfect_triangle[i][g]=left
+            triangle[i][g]=left
         else:
-            perfect_triangle[i][g]=right
-    print(perfect_triangle[-1][-1])
+            triangle[i][g]=right
+    print(triangle[-1][-1])
